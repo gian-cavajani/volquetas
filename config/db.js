f77f43fb371763887c0e453b3e38f845ed9d0f04
Gian <cavajanig@gmail.com>
2024-06-06 21:06:38 -0300

    borre variables.env

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const Sequelize = require('sequelize');
-const os = require("os")
-const hostname = os.hostname()
- 
-//si es la mac : user y pwd = '', si es win: user:postgres y pwd: Perro-2310
-const user = hostname === "MRYQ9C2014" ? '' : 'postgres'
-const pwd = hostname === "MRYQ9C2014" ? '' : 'Perro-2310'
+require('dotenv').config({ path: 'variables.env' });
 
-module.exports = new Sequelize('volquetas', user, pwd, {
-  host: '127.0.0.1',
+const user = process.env.DB_USER;
+const pwd = process.env.DB_PWD;
+const host = process.env.DB_HOST;
+const name = process.env.DB_NAME;
+
+module.exports = new Sequelize(name, user, pwd, {
+  host: host,
   port: '5432',
   dialect: 'postgres',
   pool: {
@@ -15,5 +15,5 @@ module.exports = new Sequelize('volquetas', user, pwd, {
     min: 0,
     acquire: 30000,
     idle: 10000,
-    }
-  })
+  },
+});
